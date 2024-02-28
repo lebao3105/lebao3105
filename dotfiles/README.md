a06e5fdc71faabc571b3f1ae8e6afc8a5bf98364
lebao3105 <bao12345yocoo@gmail.com>
2024-02-28 22:18:49 +0700

    Update dotfiles

--- dotfiles/README.md
+++ dotfiles/README.md
@@ -4,6 +4,9 @@ Used softwares:
 * Hyprland
 * Hyprpaper
 * Waybar
-* Mako
+* Swaync
+* Wlogout
+* Ly (display manager)
+* Alacritty
 
 Rofi theme modified from [this](https://www.reddit.com/r/unixporn/comments/18ktjh1/hyprland_asahilinux_made_my_mac_an_ultimate_dev/) (find OP's github link).
--- dotfiles/hypr/autoswitch.py
+++ dotfiles/hypr/autoswitch.py
@@ -28,35 +28,31 @@ DARK_THEME = "adw-gtk3-dark"
 LIGHT_PREFER_DEFAULT = True
 
 # Wallpaper
-LIGHT_WALL = "/home/lebao3105/Pictures/Wallpapers/grid-green-6016x6016-10269.jpg"
+LIGHT_WALL = "/home/lebao3105/Pictures/Wallpapers/10-13-6k.jpg"
 DARK_WALL = "/home/lebao3105/Pictures/Wallpapers/grid-magenta-6016x6016-10258.jpg"
 
 def main():
-    try:
-        while True:
-            now = datetime.now()
-            currtime = now.strftime("%H")
-            print("Current hour:", currtime)
+    while True:
+        now = datetime.now()
+        currtime = now.strftime("%H")
+        print("Current hour:", currtime)
+        
+        if DARK_TIME > int(currtime) >= 6:
+            print("* Time to switch to light mode!")
+            SCHEME = "prefer-light" if not LIGHT_PREFER_DEFAULT else "default"
             
-            if DARK_TIME > int(currtime) >= 6:
-                print("* Time to switch to light mode!")
-                SCHEME = "prefer-light" if not LIGHT_PREFER_DEFAULT else "default"
-                
-                os.system(f"gsettings set org.gnome.desktop.interface color-scheme '{SCHEME}'")
-                os.system(f"gsettings set org.gnome.desktop.interface gtk-theme '{LIGHT_THEME}'") 
-                
-                os.system(f"hyprctl hyprpaper wallpaper 'HDMI-A-1,{LIGHT_WALL}'")
-            else:
-                print("* Time to switch to dark mode!")
-                os.system("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
-                os.system(f"gsettings set org.gnome.desktop.interface gtk-theme '{DARK_THEME}'")
-                
-                os.system(f"hyprctl hyprpaper wallpaper 'HDMI-A-1 {DARK_WALL}'")
-            print("See you again in one hour!")
-            sleep(3600)
-    except:
-        raise
-        exit(1)
+            os.system(f"gsettings set org.gnome.desktop.interface color-scheme '{SCHEME}'")
+            os.system(f"gsettings set org.gnome.desktop.interface gtk-theme '{LIGHT_THEME}'") 
+            
+            os.system(f"hyprctl hyprpaper wallpaper 'HDMI-A-1,{LIGHT_WALL}'")
+        else:
+            print("* Time to switch to dark mode!")
+            os.system("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+            os.system(f"gsettings set org.gnome.desktop.interface gtk-theme '{DARK_THEME}'")
+            
+            os.system(f"hyprctl hyprpaper wallpaper 'HDMI-A-1 {DARK_WALL}'")
+        print("See you again in one hour!")
+        sleep(3600)
 
 if __name__ == "__main__":
     main()
